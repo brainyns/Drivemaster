61c305f79d333775cda4c2a25ebde120c540e9d8
Brainy Blanco <brainyblanco19@gmail.com>
2026-05-17 20:33:15 -0500

    fix: agregar origen de Render a CORS

--- drivemaster/src/main/java/com/drivemaster/drivemaster/security/SecurityConfig.java
+++ drivemaster/src/main/java/com/drivemaster/drivemaster/security/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174","https://drivemaster-1.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Cookie"));
         configuration.setAllowCredentials(true);
